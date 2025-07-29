dd4fdfb2b8a948658128fdab087e10f54a747052
skypper109 <colonel.diallo19@gmail.com>
2025-07-29 12:23:38 +0000

    Progression No 1

--- src/app/components/inscription/porteur-projet/porteur-projet.component.ts
+++ src/app/components/inscription/porteur-projet/porteur-projet.component.ts
@@ -1,11 +1,29 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-porteur-projet',
-  imports: [],
+  imports: [CommonModule,ReactiveFormsModule,RouterLink],
   templateUrl: './porteur-projet.component.html',
   styleUrl: './porteur-projet.component.css'
 })
 export class PorteurProjetComponent {
+  conditionUtilisation: FormGroup;
+  constructor(private fb:FormBuilder, private route:Router) {
+    this.conditionUtilisation = this.fb.group({
+      condition: [false,[Validators.requiredTrue]]
+    });
+  }
+
+  valider(){
+    if (this.conditionUtilisation.valid) {
+      console.log("Conditions acceptées");
+      // this.route.navigate(['inscription/choix']);
+    } else {
+      console.error("Veuillez accepter les conditions d'utilisation");
+    }
+  }
 
 }
